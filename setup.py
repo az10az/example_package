518bfc0b7a85261d9b6aaa3114bcbe33b71e361e
AZ <40926074+az10az@users.noreply.github.com>
2020-02-05 21:12:15 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example_package",
+    name="NCM",
     version="0.0.1",
     author="Arlens",
     author_email="email@gmail.com",
