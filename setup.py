c75dcfa01a6c8f372f6b9a422ce8e68e7653b2e7
AZ <40926074+az10az@users.noreply.github.com>
2020-02-05 20:30:19 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_hello",
+    version="0.0.1",
+    author="Cedd Burge",
+    author_email="ceddlyburge@gmail.com",
+    description="A function that returns 'hello world'",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ceddlyburge/python-hello",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+	install_requires=[
+        'python_world@git+https://github.com/ceddlyburge/python_world#egg=python_world-0.0.1',
+    ]
+)
+© 2020 GitHub
