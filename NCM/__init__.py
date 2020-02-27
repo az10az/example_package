72f37a1959cb223183fd948759ca7d731f9011b9
Arlens Zeqollari <zeqollari.arlens@gene.com>
2020-02-26 22:49:04 -0800

    Update __init__.py

--- NCM/__init__.py
+++ NCM/__init__.py
@@ -1,3 +1,6 @@
 name = "NCM"
 from . import world
 from .ingest import ingest
+from .parcel_reduce import parcel_reduce
+from .transform_data import transform_data
+from .visualization import visualization
